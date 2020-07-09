d177b2ff847dca927786414cef799bb902e6967f
Bopsi <bshangout@gmail.com>
2020-07-09 19:24:09 +0530

    CI file fix, env varbales changed

--- config/index.js
+++ config/index.js
@@ -6,35 +6,32 @@ const debug  = require("debug")("be-api:config"),
 
 dotenv.config({path: `${__dirname}/../.env`});
 
-const REDIS_HOST = process.env.REDIS_HOST;
-const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_SERVER = process.env.REDIS_SERVER.split(":");
+const REDIS_HOST   = REDIS_SERVER[0];
+const REDIS_PORT   = REDIS_SERVER[1];
+const DB_TYPE      = process.env.DATABASE_TYPE;
+const DB_URL       = `${process.env.DATABASE_SERVER}/${process.env.DATABASE}`;
+const TTL_MIN      = process.env.TTL_MIN;
+const TTL_MAX      = process.env.TTL_MAX;
 
 const client = new redis.createClient(REDIS_PORT, REDIS_HOST);
-
 client.on('error', (error) => {
     debug(error.message);
 });
-
 client.on('ready', (error) => {
     debug('Redis client Ready');
 });
 
 const subscriber = new redis.createClient(REDIS_PORT, REDIS_HOST);
-
 subscriber.on('error', (error) => {
     debug(error.message);
 });
-
 subscriber.on('ready', (error) => {
     debug('Redis subscriber Ready');
 });
 
-const DB_TYPE                     = process.env.DB_CONNECTION_TYPE;
-const DB_URL                     = process.env.DB_CONNECTION_URL;
 module.exports.host               = process.env.HOST;
 module.exports.port               = process.env.PORT;
-const TTL_MIN                     = process.env.TTL_MIN;
-const TTL_MAX                     = process.env.TTL_MAX;
 module.exports.redis              = client;
 module.exports.subscriber         = subscriber;
 module.exports.harvester          = jayson.client.http(process.env.HARVESTER);
