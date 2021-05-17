f4f1a9015173adcdd40f013da83e8b72384a3f3a
sjMalik <surajit.malik@qlikchain.com>
2021-05-17 13:00:12 +0530

    get all acounts route

--- server/routes/account.js
+++ server/routes/account.js
@@ -6,9 +6,32 @@ const debug   = require("debug")("be-api:transactions"),
 const config          = require("../../config");
 const router          = express.Router();
 
+router.route('')
+    .get(async (req, res) => {
+        debug(`GET - /accounts`);
+        let page    = numeral(req.query.page || 0).value();
+        let perPage = numeral(req.query.perPage || 10).value();
+
+        let total = 0;
+
+        try {
+            const store = await config.dataStore.getStore();
+
+            let signers = await store.transaction.getSigners(page, perPage);
+
+            return res.status(200).send(signers).end();
+        } catch(e) {
+            debug(e);
+            return res.status(200).send({
+                err: e,
+                msg: "Internal Server Error"
+            }).end();
+        }
+    });
+
 router.route('/:address')
     .get(async (req, res) => {
-        debug(`GET - /address/${req.params.address}`);
+        debug(`GET - /accounts/${req.params.address}`);
         let page    = numeral(req.query.page || 0).value();
         let perPage = numeral(req.query.perPage || 10).value();
 
