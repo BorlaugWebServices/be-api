e14b87b6308f803f31e839c88f59326cebd324fe
sjMalik <surajit.malik@qlikchain.com>
2022-01-25 18:35:05 +0530

    change in group response

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -6,6 +6,7 @@ const debug = require("debug")("be-api:groups"),
     express = require("express");
 
 const config = require("../../config");
+const transaction      = require("./transactions");
 const router = express.Router();
 const GROUP_ID_PATTERN = RegExp('^[0-9]*$');
 
@@ -21,17 +22,53 @@ router.get('/:groupid', async (req, res) => {
         const store = await config.dataStore.getStore();
         let group_from_db   = await store.group.get(groupid);
         let group = await config.harvester.request('getGroup', {group_id: groupid});
+        debug("Group", group);
         if (group.result && group_from_db) {
-            group.result.group['blockNumber'] = group_from_db.blockNumber;
-            group.result.group['blockHash'] = group_from_db.blockHash;
-            group.result.group['extrinsicHash'] = group_from_db.extrinsicHash;
-            group.result.group['timestamp'] = group_from_db.timestamp;
+            group.result['blockNumber'] = group_from_db.blockNumber;
+            group.result['blockHash'] = group_from_db.blockHash;
+            group.result['extrinsicHash'] = group_from_db.extrinsicHash;
+            group.result['timestamp'] = group_from_db.timestamp;
         }
-        return res.status(200).send(group).end();
+        return res.status(200).send(group.result).end();
     } catch (e) {
         debug(e);
         return res.status(200).send({
             err: e,
+            msg: "Internal Server Error"
+        }).end();
+    }
+});
+
+router.get('/:groupid/activities', async (req, res) => {
+    let groupid = req.params.groupid;
+    debug(`GET - /groups/${groupid}/activities`);
+
+    if(!GROUP_ID_PATTERN.test(groupid)) {
+        return res.status(404).send({msg: `Invalid group id`}).end();
+    }
+
+    try {
+        const store = await config.dataStore.getStore();
+
+        let activityKeys = await store.group.getActivities(groupid);
+        let activities   = activityKeys.length > 0 ? await store.transaction.getList(activityKeys) : [];
+
+        /**** TEST ****/
+        let calls = [];
+        activities.forEach(act => {
+            calls.push(transaction.getTransactionStatus(act));
+        });
+        let statuses = await Promise.all(calls);
+        activities.forEach((e, i, a) => {
+            a[i]["isSuccess"] = statuses[i];
+        });
+        /************/
+
+        return res.status(200).send(activities).end();
+    } catch(e) {
+        debug(e);
+        return res.status(500).send({
+            err: e.message,
             msg: "Internal Server Error"
         }).end();
     }
