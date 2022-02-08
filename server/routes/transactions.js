c414bcb172e8bf90676f3dffa97c4432419742d1
sjMalik <surajit.malik@qlikchain.com>
2022-02-08 19:15:30 +0530

    get weight in txs

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -30,7 +30,12 @@ router.route('/')
             let depositEvent = _.find(events, (event) => {
                 return event.meta.name === "Deposit";
             });
+            let extrinsicSuccessEvent = _.find(events, (event) => {
+                return event.meta.name === "ExtrinsicSuccess";
+            });
+            debug(extrinsicSuccessEvent.event.data)
             txns.slice[i]['tx_fee'] = depositEvent ? Number(depositEvent.event.data[0]) : 0;
+            txns.slice[i]['weight'] = extrinsicSuccessEvent ? Number(extrinsicSuccessEvent.event.data[0].weight) : 0;
         }
 
         return res.status(200).send(txns).end();
